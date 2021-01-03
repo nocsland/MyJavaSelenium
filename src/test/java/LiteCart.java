caf30d1e491a1a48e697054aa2f4a819a28147d3
Anton Vlasov <nocsland@gmail.com>
2021-01-03 22:19:26 +0400

    Add LiteCart

--- src/test/java/LiteCart.java
+++ src/test/java/LiteCart.java
@@ -1,12 +1,7 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
 
 
