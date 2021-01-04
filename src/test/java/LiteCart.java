542304fdcb83e9a9b0dd8929053d6cfded693ab4
Anton Vlasov <nocsland@gmail.com>
2021-01-04 14:14:59 +0400

    Добавлена проверка
    Добавлен метод isElementPresent, добавлена проверка (assertTrue) ожидания элемента

--- src/test/java/LiteCart.java
+++ src/test/java/LiteCart.java
@@ -1,13 +1,16 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.opera.OperaDriver;
-
-
-import java.io.File;
 import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertTrue;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 
 public class LiteCart {
@@ -18,7 +21,7 @@ public class LiteCart {
 
     public void start() throws Exception {
         driver = new OperaDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
     }
 
@@ -30,17 +33,27 @@ public class LiteCart {
         driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[1]/div[1]/div/input")).sendKeys("admin");
         driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[1]/div[2]/div/input")).sendKeys("admin");
         driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[2]/button")).click();
+        assertTrue(isElementPresent(By.xpath("//*[@id=\"logotype\"]/a/img")));
         driver.findElement(By.xpath("//*[@id=\"top-bar\"]/li[10]/a")).click();
-
 
 
     }
 
+
     @After
 
     public void stop() {
-      driver.quit();
-      driver = null;
+        driver.quit();
+        driver = null;
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 }
 
