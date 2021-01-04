cc600fe02b227d0573154f6629b80f87b1ba873c
Anton Vlasov <nocsland@gmail.com>
2021-01-04 17:35:00 +0400

    Добавлены измениния связанные с wait
    1. добавлен в мой первый тест объект wait
    2. добавлено ожидание тайтла
    3. изменена кодировка класса MyFirstTest на 1251

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -2,49 +2,50 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebDriver;;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
-
-
-import java.io.File;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 
 public class MyFirstTest {
     private WebDriver driver;
-
+    private WebDriverWait wait;
 
 
     @Before
 
     public void start() throws Exception {
         driver = new OperaDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver,2);
+        //driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
     }
 
     @Test
 
-    public void MyFirstTest(){
+    public void MyFirstTest() {
 
         driver.get("http://www.google.com");
         driver.findElement(By.name("q")).clear();
         driver.findElement(By.name("q")).sendKeys("test");
-        driver.findElement(By.id("tsf")).submit();
+        driver.findElement(By.name("btnK")).submit();
+        wait.until(titleIs("test - Поиск в Google"));
 
     }
 
+
     @After
 
-    public void stop(){
+    public void stop() {
         driver.quit();
         driver = null;
 
     }
 
-
 }
+
